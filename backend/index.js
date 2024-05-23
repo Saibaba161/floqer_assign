b13ddb8ef59bc722edd64a4b9fdea4592e8f66b7
Saibaba-1610 <sai1610k@gmail.com>
2024-05-24 00:20:09 +0530

    unable to fix

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use(cors(
     {
-        origin: ['https://floqer-assign-frontend.vercel.app/','https://floqer-assign-backend.vercel.app/api/data/:year','https://floqer-assign-backend.vercel.app/api/data/'],
+        origin: ['https://floqer-assign-frontend.vercel.app/','https://floqer-assign-backend.vercel.app/api/data/:year'],
         methods: ["POST", "GET", "OPTIONS"],
         credentials: true,
         allowedHeaders: 'Content-Type:application/json'
