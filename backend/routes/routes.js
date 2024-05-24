3fd68ec81f43b4ba6b213df86441cd098a0b5168
Saibaba-1610 <sai1610k@gmail.com>
2024-05-24 16:08:35 +0530

    fixing

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-app.all('/', function(req, res, next) {
+/*app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "https://floqer-assign-frontend.vercel.app/");
     next()
 });
+*/
 
 //controller functions
 const { getData, getSingleData } = require('../controller')
